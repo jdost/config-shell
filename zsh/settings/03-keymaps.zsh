d1ed39718bbdb34b009962ef2342a5ea17aad3c2
Jeff <jostendorf@gmail.com>
2013-05-12 09:03:52 -0500

    Added terminfo support to keys

--- zsh/settings/03-keymaps.zsh
+++ zsh/settings/03-keymaps.zsh
@@ -1,12 +1,11 @@
 # Key bindings
-bindkey "^[[7~"  beginning-of-line    # Home
-bindkey "^[[8~"  end-of-line          # End
-bindkey "^[[5~"  beginning-of-history # PageUp
-bindkey "^[[6~"  end-of-history       # PageDown
-bindkey "^[[3~"  delete-char          # Del
+bindkey "${terminfo[khome]}"  beginning-of-line    # Home
+bindkey "${terminfo[kend]}"   end-of-line          # End
+bindkey "${terminfo[kpp]}"    beginning-of-history # PageUp
+bindkey "${terminfo[knp]}"    end-of-history       # PageDown
+bindkey "${terminfo[kdch1]}"  delete-char          # Del
 bindkey "^[0c"   forward-word         # Ctrl + Right
 bindkey "^[0d"   backward-word        # Ctrl + Left
-bindkey "^[0c~"  beginning-of-line
 
 # vim bindings
 bindkey -M vicmd "/" history-incremental-search-backward
