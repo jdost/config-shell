b960cdb4b460209f1a13ce3587a3cefa4522fb21
Jeff <jostendorf@gmail.com>
2013-05-20 07:57:04 -0500

    Backspace/Delete key is bound to opposite of the . action
    This will delete the full `..` or `/..` that is expanded by the rationalize_dot
    function

--- zsh/settings/03-keymaps.zsh
+++ zsh/settings/03-keymaps.zsh
@@ -30,3 +30,19 @@ function rationalize_dot {
 zle -N rationalize_dot
 bindkey . rationalize_dot
 bindkey -M isearch . self-insert
+
+function unrationalize_dot {
+   local MATCH # keep regex match from leaking into the environment
+   if [[ $LBUFFER =~ '(^|/| |      |'$'\n''|\||;|&)\.\.$' ]]; then
+      zle backward-delete-char
+      zle backward-delete-char
+      if [[ $MATCH =~ '^/' ]]; then
+         zle backward-delete-char
+      fi
+   else
+      zle backward-delete-char
+   fi
+}
+zle -N unrationalize_dot
+bindkey "^h" unrationalize_dot
+bindkey "^?" unrationalize_dot
